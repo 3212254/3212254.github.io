b13040bd542ec95fdf17c1eac89db1c4f1bc5177
3212254 <3212254@gmail.com>
2019-07-14 16:13:27 +0300

    commit

--- 6menu/assets/js/script.js
+++ 6menu/assets/js/script.js
@@ -4,3 +4,51 @@ $(document).ready(function() {
     $(this).toggleClass('nav-link-show');
   });
 });
+
+
+
+var map;
+
+        // Функция initMap которая отрисует карту на странице
+        function initMap() {
+
+            // В переменной map создаем объект карты GoogleMaps и вешаем эту переменную на <div id="map"></div>
+            map = new google.maps.Map(document.getElementById('map'), {
+                // При создании объекта карты необходимо указать его свойства
+                // center - определяем точку на которой карта будет центрироваться
+                center: {lat: -34.397, lng: 150.644},
+                // zoom - определяет масштаб. 0 - видно всю платнеу. 18 - видно дома и улицы города.
+                zoom: 8
+            });
+        }
+
+// Создаем маркер на карте
+var marker = new google.maps.Marker({
+
+    // Определяем позицию маркера
+    position: {lat: 52.441059, lng: 30.990512},
+
+    // Указываем на какой карте он должен появится. (На странице ведь может быть больше одной карты)
+    map: map,
+
+    // Пишем название маркера - появится если навести на него курсор и немного подождать
+    title: 'Наш маркер: Большой театр'
+});
+
+
+
+
+
+
+function initMap() {
+    var coordinates = {lat: 52.441059, lng: 30.990512},
+
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: coordinates
+        }),
+    
+        marker = new google.maps.Marker({
+            position: coordinates,
+            map: map
+        });
+}
